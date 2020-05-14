092bd2b2a8a3cb0fad02a6d997c0d3df55c628e9
pogc <eduardo.s.fernandes@tecnico.ulisboa.pt>
2020-05-14 19:00:16 +0100

    Actual first command

--- bot.py
+++ bot.py
@@ -1,13 +1,18 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.')
+client = commands.Bot(command_prefix = 'hive.')
 
 
 @client.event
 async def on_ready():
     print('Bot is ready')
     print('Smile')
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong! {}ms'.format(round(client.latency * 1000)))
 
 
 with open("config.ini", "r+") as file:
