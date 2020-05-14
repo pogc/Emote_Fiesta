03f92a502e85adf68d9bdb23cb9fd0dd8064ea31
sabre183 <djsoundnr1@gmail.com>
2020-05-14 18:17:08 +0200

    :(

--- bot.py
+++ bot.py
@@ -14,4 +14,4 @@ with open("config.ini", "r+") as file:
     client.run(file.readline())
 
 # This is a change :)
-# No :)
+# Cock and ball torture (CBT), penis torture or dick torture is a sexual activity involving application of pain or constriction to the penis or testicles. This may involve directly painful activities, such as genital piercing, wax play, genital spanking, squeezing, ball-busting, genital flogging, urethral play, tickle torture, erotic electrostimulation, kneeing or kicking.[1] The recipient of such activities may receive direct physical pleasure via masochism, or emotional pleasure through erotic humiliation, or knowledge that the play is pleasing to a sadistic dominant. Many of these practices carry significant health risks
