8eb8eff3497248f08cb4d0aba9f1383193ddea36
pogc <eduardo.s.fernandes@tecnico.ulisboa.pt>
2020-05-16 17:48:30 +0100

    Initial setup for cogs (classes)

--- bot.py
+++ bot.py
@@ -1,24 +1,23 @@
 import discord
+import os
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = 'hive.')
 
 
-@client.event
-async def on_ready():
-    print('Bot is ready')
-    print('Smile')
+@client.command()
+async def load(ctx, extension):
+    client.load_extension('cogs.{}'.format(extension))
 
 
 @client.command()
-async def ping(ctx):
-    await ctx.send('Pong! {}ms'.format(round(client.latency * 1000)))
+async def unload(ctx, extension):
+    client.unload_extension('cogs.{}'.format(extension))
 
 
-@client.command(aliases = ['8ball', 'text'])
-async def _8ball(ctx, *, question):
-    response = 'KEKW'
-    await ctx.send('Question: {}\nAnswer: {}'.format(question, response))
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension('cogs.{}'.format(filename[:-3]))
 
 
 with open("config.ini", "r+") as file:
---
+++ cogs/example.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+class Example(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    #Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Bot is ready')
+        print('Smile')
+
+    #Commands
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send('Pong! {}ms'.format(round(self.client.latency * 1000)))
+
+    @commands.command(aliases = ['8ball', 'text'])
+    async def _8ball(self, ctx, *, question):
+        response = 'KEKW'
+        await ctx.send('Question: {}\nAnswer: {}'.format(question, response))
+
+
+def setup(client):
+    client.add_cog(Example(client))
